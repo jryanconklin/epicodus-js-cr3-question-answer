6922f643d7da3391d3311a4d94febc450e0d33df
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-28 14:38:52 -0700

    Refactor for Delete and Update Only on Detail Component

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,17 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-
-    delete(question) {
-      if(confirm('For Realsies?')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    },
-
-    update(question, params) {
-      this.sendAction('update', question, params);
-    }
-
 //End
   }
 });
