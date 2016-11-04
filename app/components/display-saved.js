ce7ef533f2499fb0330d2e4fc46485168b1824ce
J. Ryan Conklin <jryanconklin@gmail.com>
2016-11-04 11:32:16 -0700

    Add Save Questions Service

---
+++ app/components/display-saved.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  questionSave: Ember.inject.service(),
+
+
+}); //End Ember Component
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+
+  questionSave: Ember.inject.service('question-save'),
+
+  actions: {
+    addSaved(item) {
+      this.get('questionSave').add(item);
+    },
+
+
+  } // End Actions
+
+}); // End Component
